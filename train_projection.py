cdf54da9b93e8d1289b7bcb3ab61911b94e68221
samrat-halder <samrathalder18@gmail.com>
2020-12-18 01:12:44 +0000

    exclude large batches to avoid OOM while training on batch

--- train_projection.py
+++ train_projection.py
@@ -15,12 +15,16 @@ from collections import Counter
 import scipy.sparse as sp
 from operator import itemgetter
 import numpy as np
+import glob
+import pickle
+import os
 
 import tensorflow as tf
 from keras import backend as K
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 config.log_device_placement = True
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 if __name__ == '__main__':
@@ -76,35 +80,49 @@ if __name__ == '__main__':
     #build the model
     np_sg_model = model_skip_gram(projection_dim=1120, emb_dim=100)
     id2word = load_pickle( './data', f'id2w_{n}_{data}.pkl')
-    skip_grams = load_pickle('./data', f'skip_grams_{n}_{data}.pkl')
+    # skip_grams = load_pickle('./data', f'skip_grams_{n}_{data}.pkl')
 
     print('model training started...')
     session = tf.compat.v1.Session(config=config)
     tf.compat.v1.keras.backend.set_session(session)
     #train
+    all_batch = [f for f in glob.glob(f'./data/skip_grams_{n}_{data}/*.pkl')]
     for epoch in range(1, num_epoch+1):
         print('Epoch:', epoch)
         loss = 0
-        for i, elem in enumerate(skip_grams):
-            pair_first_elem = np.array(list(zip(*elem[0]))[0], dtype='int32')
-            pair_second_elem = np.array(list(zip(*elem[0]))[1], dtype='int32')
-            first_words_to_hash = [list(itemgetter(*pair_first_elem)(id2word))]
-            before_1 = pipeline.named_steps["char_term_frequency"].transform(first_words_to_hash)
-            after_1 = pipeline.named_steps["union"].transform(before_1)
+        batch_id = 1
+        for skip_gram_batch in all_batch:
+            with open(skip_gram_batch, 'rb') as f:
+                skip_grams = pickle.load(f)
+            f.close()
 
-            second_words_to_hash = [list(itemgetter(*pair_first_elem)(id2word))]
-            before_2 = pipeline.named_steps["char_term_frequency"].transform(second_words_to_hash)
-            after_2 = pipeline.named_steps["union"].transform(before_2)
+            for i, elem in enumerate(skip_grams):
+                # print(i)
+                pair_first_elem = np.array(list(zip(*elem[0]))[0], dtype='int32')
+                pair_second_elem = np.array(list(zip(*elem[0]))[1], dtype='int32')
+                first_words_to_hash = [list(itemgetter(*pair_first_elem)(id2word))]
+                before_1 = pipeline.named_steps["char_term_frequency"].transform(first_words_to_hash)
+                after_1 = pipeline.named_steps["union"].transform(before_1)
 
-            labels = np.array(elem[1], dtype='int32')
-            X = [after_1[0].toarray(), after_2[0].toarray()]
-            Y = labels
+                second_words_to_hash = [list(itemgetter(*pair_first_elem)(id2word))]
+                before_2 = pipeline.named_steps["char_term_frequency"].transform(second_words_to_hash)
+                after_2 = pipeline.named_steps["union"].transform(before_2)
 
-            if i % 100 == 0 and i != 0:
-                print(f'\tProcessed skip-grams from {i} sentences | Loss: {loss}')
-            loss += np_sg_model.train_on_batch(X,Y) 
+                labels = np.array(elem[1], dtype='int32')
+                X = [after_1[0].toarray(), after_2[0].toarray()]
+                Y = labels
 
-    del skip_grams
+                if len(Y) > max_batch_size:
+                    print(f'Warning: number of skip-grams in this batch {i} is {len(Y)} > {max_batch_size}. To avoid OOM skipping this batch')
+                    continue
+
+                # print(len(Y))
+                # if i % 100 == 0 and i != 0:
+                loss += np_sg_model.train_on_batch(X,Y)
+
+            print(f'Loss: {loss} after processing bacth_id {batch_id}')
+            batch_id += 1
+
     tf.compat.v1.keras.backend.get_session().close()
 
     #prepare embedding matrix for DAN training
